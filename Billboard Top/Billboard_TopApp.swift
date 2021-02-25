218dc1c8552d328bf4cc8a71732f23261c45642c
Devnanda Manjunadh <mdevnanda@gmail.com>
2021-02-25 10:18:22 -0600

    Initial Commit

---
+++ Billboard Top/Billboard_TopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Billboard_TopApp.swift
+//  Billboard Top
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Billboard_TopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
